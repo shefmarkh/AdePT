76daa37f2d1a7d095ea3358194ff73c2acbeb130
Witek Pokorski <witold.pokorski@cern.ch>
2020-09-23 17:27:25 +0200

    preparing for pair production

--- test/cufisher_price.cu
+++ test/cufisher_price.cu
@@ -7,10 +7,15 @@
 
 // kernel function that generates the 'shower'
 __global__
-void shower(int n, float *particle_energy)
-{
+void shower(int n, float *particle_energy, float *totalEnergyLoss, int *numberOfSecondaries)
+ {
  float r;
  curandState state;
+
+ curand_init(0, /* the seed controls the sequence of random values that are produced */
+             0, /* the sequence number is only important with multiple cores */
+             0, /* the offset is how much extra we advance in the sequence for each call, can be 0 */
+             &state);
 
  while (n>=0)
   {
@@ -20,8 +25,20 @@ void shower(int n, float *particle_energy)
 
      if (r < 0.5f)
       {
-       particle_energy[n] = 0;
+       float eloss = 0.2f * particle_energy[n];
+       *totalEnergyLoss += (eloss < 0.001f ? particle_energy[n] : eloss);
+       particle_energy[n] = (eloss < 0.001f ? 0.0f : (particle_energy[n] - eloss));
       }
+     else
+     {
+      float eloss = 0.5f * particle_energy[n];
+      //
+      // here I need to create a new particle
+      //
+      numberOfSecondaries++;
+
+      particle_energy[n] -= eloss;
+     }
     }
     n--;
   }
@@ -34,20 +51,27 @@ int main()
  int N = 1<<20;
  float *particle_energy;
 
+ float *totalEnergyLoss;
+ int *numberOfSecondaries;
 
  // Allocate Unified Memory accessible from CPU or GPU
  cudaMallocManaged(&particle_energy, N*sizeof(float));
+ cudaMallocManaged(&totalEnergyLoss, sizeof(float));
+ cudaMallocManaged(&numberOfSecondaries, sizeof(int));
+
+ *totalEnergyLoss = 0;
+ *numberOfSecondaries = 0;
 
  // initialize particle_energy array on the host
   for (int i = 0; i < N; i++) 
    {
-    particle_energy[i] = 1.0f;
+    particle_energy[i] = 100.0f;
    }
 
-  int n_particles = 100;
+  int n_particles = 1000;
 
   // Run kernel on 100 particles on the GPU
-  shower<<<1, 1>>>(n_particles, particle_energy);
+  shower<<<1, 1>>>(n_particles, particle_energy, totalEnergyLoss, numberOfSecondaries);
 
   // Wait for GPU to finish before accessing on host
   cudaDeviceSynchronize();
@@ -55,11 +79,13 @@ int main()
   // check that the final energy of all particles is 0
   for (int i = 0; i < n_particles; i++)
   {
-    std::cout << "energy of particle " << i << " is " << particle_energy[i] << std::endl;
+  //  std::cout << "energy of particle " << i << " is " << particle_energy[i] << std::endl;
   }
+
+  std::cout << "Total energy loss " << *totalEnergyLoss << " number of secondaries " << *numberOfSecondaries << std::endl;
 
   // Free memory
   cudaFree(particle_energy);
-
-  return 0;
+  cudaFree(totalEnergyLoss);
+  cudaFree(numberOfSecondaries);
 }
