b72384fe2c032f37c256d967311dc1c58da43767
Witek Pokorski <witold.pokorski@cern.ch>
2020-09-23 16:37:01 +0200

    adding rnd number generation

--- test/cufisher_price.cu
+++ test/cufisher_price.cu
@@ -2,28 +2,40 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include <curand.h>
+#include <curand_kernel.h>
 #include <iostream>
 
 // kernel function that generates the 'shower'
 __global__
 void shower(int n, float *particle_energy)
 {
+ float r;
+ curandState state;
+
  while (n>=0)
   {
    while (particle_energy[n]>0)
     {
-     particle_energy[n] = 0;
+     r = curand_uniform(&state);
+
+     if (r < 0.5f)
+      {
+       particle_energy[n] = 0;
+      }
     }
     n--;
   }
-}
+ } 
+
+//////// 
 
 int main()
 {
  int N = 1<<20;
  float *particle_energy;
 
- // Allocate Unified Memory – accessible from CPU or GPU
+
+ // Allocate Unified Memory accessible from CPU or GPU
  cudaMallocManaged(&particle_energy, N*sizeof(float));
 
  // initialize particle_energy array on the host
